e43d889904d86659e3008c913079160808b5c425
Christopher William Ruz Olate <christopher.ruz@bci.cl>
2025-11-18 19:39:14 -0300

    feature: Add lazy column to display the item list refactor: Text component displays the item data

--- app/src/main/java/com/example/androidplayground/main/screen/MainScreen.kt
+++ app/src/main/java/com/example/androidplayground/main/screen/MainScreen.kt
@@ -1,22 +1,31 @@
 package com.example.androidplayground.main.screen
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidplayground.common.data.AppDatabase
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     onItemClick: (Int) -> Unit = {}
 ) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier.clickable {
-            onItemClick(1)
+    val fakeItems = AppDatabase.getFakeItems()
+
+    LazyColumn {
+        items(fakeItems) { item ->
+            Text(
+                text = "${item.name}\n${item.description}",
+                modifier = modifier.clickable {
+                    onItemClick(item.id)
+                }
+            )
         }
-    )
+    }
 }
 
 @Composable
