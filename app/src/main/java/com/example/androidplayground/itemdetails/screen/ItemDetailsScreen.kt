493648bd150c6364878c81e382d7006f12b0739a
Christopher William Ruz Olate <christopher.ruz@bci.cl>
2025-11-17 21:45:40 -0300

    chore: Add preview for main and detail screen

--- app/src/main/java/com/example/androidplayground/itemdetails/screen/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/androidplayground/itemdetails/screen/ItemDetailsScreen.kt
@@ -3,11 +3,19 @@ package com.example.androidplayground.itemdetails.screen
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ItemDetailsScreen(itemId: Int, modifier: Modifier) {
+fun ItemDetailsScreen(itemId: Int, modifier: Modifier = Modifier) {
     Text(
         text = "Item Screen $itemId",
         modifier = modifier
     )
+}
+
+
+@Composable
+@Preview(showSystemUi = true)
+fun ThisPreview() {
+    ItemDetailsScreen(itemId = 1)
 }
